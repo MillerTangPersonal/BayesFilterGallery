06544052063724fa439705c28c3bf9cbd3e9c4b0
wenda <wenda.zhao@robotics.utias.utoronto.ca>
2021-11-15 16:18:24 -0500

    Add the bias est for real data

--- gallery/eskf/plot_util.py
+++ gallery/eskf/plot_util.py
@@ -4,7 +4,10 @@ plotting functions
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import linalg
 from scipy.stats import norm
+from scipy import interpolate
+from pyquaternion import Quaternion
 import matplotlib.mlab as mlab
 import math
 import matplotlib
@@ -106,3 +109,103 @@ def plot_traj(pos_vicon, Xpo, anchor_pos):
     plt.title(r"Trajectory of the experiment", fontsize=FONTSIZE, fontweight=0, color='black', style='italic', y=1.02 )
     
 
+# split TDOA meas and get the corresponding gt
+def getTDOA_gt(t_uwb, tdoa, uwb_p, anchor_pos):
+
+    # combine uwb tdoa timestep and meas. for visualization
+    tdoa = np.concatenate((t_uwb, tdoa),axis=1) 
+
+    # get the id for tdoa_ij measurements
+    tdoa_meas_70 = np.where((tdoa[:,1]==[7])&(tdoa[:,2]==[0]))
+    tdoa_70 = np.squeeze(tdoa[tdoa_meas_70, :])
+
+    tdoa_meas_01 = np.where((tdoa[:,1]==[0])&(tdoa[:,2]==[1]))
+    tdoa_01 = np.squeeze(tdoa[tdoa_meas_01, :])
+
+    tdoa_meas_12 = np.where((tdoa[:,1]==[1])&(tdoa[:,2]==[2]))
+    tdoa_12 = np.squeeze(tdoa[tdoa_meas_12, :])
+
+    tdoa_meas_23 = np.where((tdoa[:,1]==[2])&(tdoa[:,2]==[3]))
+    tdoa_23 = np.squeeze(tdoa[tdoa_meas_23, :])
+
+    tdoa_meas_34 = np.where((tdoa[:,1]==[3])&(tdoa[:,2]==[4]))
+    tdoa_34 = np.squeeze(tdoa[tdoa_meas_34, :])
+
+    tdoa_meas_45 = np.where((tdoa[:,1]==[4])&(tdoa[:,2]==[5]))
+    tdoa_45 = np.squeeze(tdoa[tdoa_meas_45, :])
+
+    tdoa_meas_56 = np.where((tdoa[:,1]==[5])&(tdoa[:,2]==[6]))
+    tdoa_56 = np.squeeze(tdoa[tdoa_meas_56, :])
+
+    tdoa_meas_67 = np.where((tdoa[:,1]==[6])&(tdoa[:,2]==[7]))
+    tdoa_67 = np.squeeze(tdoa[tdoa_meas_67, :])
+
+    tdoa_meas = np.array([tdoa_70, tdoa_01, tdoa_12, tdoa_23,
+                          tdoa_34, tdoa_45, tdoa_56, tdoa_67], dtype=object)
+    # compute the ground truth for tdoa_ij
+    d = []
+    for i in range(8):
+        d.append(linalg.norm(anchor_pos[i,:].reshape(1,-1) - uwb_p, axis = 1))
+    
+    # shape of d is 8 x n
+    d = np.array(d)
+    # measurement model
+    d_70 = d[0,:] - d[7,:]; d_01 = d[1,:] - d[0,:]; d_12 = d[2,:] - d[1,:]; d_23 = d[3,:] - d[2,:]
+    d_34 = d[4,:] - d[3,:]; d_45 = d[5,:] - d[4,:]; d_56 = d[6,:] - d[5,:]; d_67 = d[7,:] - d[6,:]
+    
+    d_gt = np.array([d_70, d_01, d_12, d_23, d_34, d_45, d_56, d_67], dtype=object)
+
+    return tdoa_meas, d_gt
+
+# visualize bias (by id)
+def visual_bias(tdoa_meas, d_gt, t_vicon, t, eskf, id):
+    gt_time = t_vicon
+    # get synchronized gt
+    d_int = interp_gt(gt_time, d_gt[id], tdoa_meas[id][:,0])
+    # bias = tdoa - gt
+    bias = tdoa_meas[id][:,3] - d_int
+
+    plt.figure()
+    if id == 0:
+        plt.title(r"UWB TDOA meas biass, (An7, An0)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 1:
+        plt.title(r"UWB TDOA meas biass, (An0, An1)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 2:
+        plt.title(r"UWB TDOA meas biass, (An1, An2)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 3:
+        plt.title(r"UWB TDOA meas biass, (An2, An3)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 4:
+        plt.title(r"UWB TDOA meas biass, (An3, An4)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 5:
+        plt.title(r"UWB TDOA meas biass, (An4, An5)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 6:
+        plt.title(r"UWB TDOA meas biass, (An5, An6)", fontsize=FONTSIZE, fontweight=0, color='black')
+    elif id == 7:
+        plt.title(r"UWB TDOA meas biass, (An6, An7)", fontsize=FONTSIZE, fontweight=0, color='black')
+
+    plt.scatter(tdoa_meas[id][:,0], bias, color = "steelblue", s = 6.5, alpha = 0.9, label = "tdoa biases")
+    plt.plot(t, eskf.Xpo[:,6+id], color = "orange", label = "est. biases")
+    plt.legend(loc='best',fontsize = FONTSIZE)
+    plt.xlabel(r'Time [s]',fontsize = FONTSIZE)
+    plt.ylabel(r'TDOA bias [m]',fontsize = FONTSIZE) 
+    plt.ylim([-0.7, 0.7]) 
+
+# interpolate gt meas. using UWB time
+def interp_gt(gt_time, gt_m, tdoa_time):
+    f_gt = interpolate.splrep(gt_time, gt_m, s = 0.5)
+    # synchronized gt meas.
+    m_int = interpolate.splev(tdoa_time, f_gt, der = 0)
+    return m_int
+
+def get_uwbp(gt_pose):
+    # gt_pose: [x, y, z, qx, qy, qz, qw]
+    # external calibration: convert the gt_position to UWB antenna center
+    t_uv = np.array([-0.01245, 0.00127, 0.0908]).reshape(-1,1) 
+    uwb_p = np.zeros((len(gt_pose), 3))
+    for idx in range(len(gt_pose)):
+        q_cf =Quaternion([gt_pose[idx,6], gt_pose[idx,3], gt_pose[idx,4], gt_pose[idx,5]])    # [q_w, q_x, q_y, q_z]
+        C_iv = q_cf.rotation_matrix                       # rotation matrix from vehicle body frame to inertial frame
+        uwb_ac = C_iv.dot(t_uv) + gt_pose[idx,0:3].reshape(-1,1)
+        uwb_p[idx,:] = uwb_ac.reshape(1,-1)     # gt of uwb tag
+        
+    return uwb_p
